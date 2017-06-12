5bf3b87003aac5845b6d4c50514766352aa0ff0c
Francis Nduba Numbi <francisnnumbi@yandex.com>
2017-06-12 03:29:28 +0200

    using recyclerview and cardview.  moving from dialog to activity, replacing gridview by table, and other changes

--- src/main/java/fnn/smirl/format/drc/constitution/core/Articles.java
+++ src/main/java/fnn/smirl/format/drc/constitution/core/Articles.java
@@ -1,7 +1,7 @@
 package fnn.smirl.format.drc.constitution.core;
 import java.util.ArrayList;
 
-public class Articles
-{
- public ArrayList<Article> list = new ArrayList<Article>();
-}
+public class Articles {
+	public long versionId = 0;
+	public ArrayList<Article> list = new ArrayList<Article>();
+ }
---
+++ src/main/java/fnn/smirl/format/drc/constitution/core/Utils.java
@@ -0,0 +1,10 @@
+package fnn.smirl.format.drc.constitution.core;
+import java.util.*;
+
+public class Utils
+{
+ 
+ public static long getDateVersion(){
+	return Calendar.getInstance().getTimeInMillis();
+ }
+}
